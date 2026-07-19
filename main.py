2c3fb1f2dd250a0433d56b29e1a9e162e081f683
palparch <tuilipshrm2008@gmail.com>
2026-07-20 03:34:32 +0530

    fixed code structure, except for some bugs which are not really my job ig

--- main.py
+++ main.py
@@ -3,29 +3,32 @@ import matplotlib.pyplot as plt
 import csv
 
 #print menu
-print("\n -*10 HOSPITAL MANAGEMENT SYSTEM -*10)
-print(" please select an option ")
+print("-"*10, "HOSPITAL MANAGEMENT SYSTEM", "-"*10)
+print("~ please select an option ~")
 print("1. Display All Records ")
 print("2. Add a New Patient Record ")
 print("3. Search Patient with ID")
 print("4. Delete Patient Record ")
-print("5. Calculate Bill ")
+print("5. Cialculate Bill ")
 print("6. Display Graph ")
 print("7. Exit Program ")
-print(" -*20")
+print("-"*20)
 
 choice = int(input("Enter your choice: "))
 
-display_records():
+def display_records():
 	df = pd.read_csv("patient.csv")
 	print("\nPatient Records")
 	print(df)
 
-add_patient():
-	pid = int(input("Patient ID: "))
+def add_patient():
+	try:
+		pid = int(input("Patient ID: "))
+	except:
+		print("Please enter a valid value. PID can only be an integer value.")
 	name = input("Name: ")
 	age = int(input("Age: "))
-	gender = input("Gender: "))
+	gender = input("Gender: ")
 	diseases = input("Diseases: ")
 	doctor = input("Doctor: ")
 	bill = int(input("Bill: "))
@@ -36,10 +39,11 @@ add_patient():
 	df.to_csv("patient.csv", index=False)
 	print("\nPatient Successfully Added")
 
-search_patient():					#ye tu karegi tulip
-delete_patient():					#ye tu karegi tulip
-calc_bill():						#ye tu karegi tulip
-display_garph():
+#def search_patient():					#ye tu karegi tulip
+#def delete_patient():					#ye tu karegi tulip
+#def calc_bill():						#ye tu karegi tulip
+def display_graph():
+	df = pd.read_csv("patient.csv")
 	diseases_count = df["Diseases"].value_counts()
 	
 	plt.figure(figure=(8,5))
@@ -63,6 +67,23 @@ display_garph():
 	plt.show()
 
 
-exit_program():
+def exit_program():
 	print("Thank You")
-	break
+
+
+if choice == 1:
+	display_records()
+elif choice == 2:
+	add_patient()
+elif choice == 3:
+	search_patient()
+elif choice == 4:
+	delete_patient()
+elif choice == 5:
+	calc_bill()
+elif choice == 6:
+	display_graph()
+elif choice == 7:
+	exit_program()
+else:
+	print("Please enter a valid choice.")
